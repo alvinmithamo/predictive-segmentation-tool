d7c10c4991cdea332a1408317227d9185c0d4759
alvinmithamo <alvinmithamo1@gmail.com>
2026-02-27 11:03:11 +0300

    configure Pydantic settings and environment variables

---
+++ backend/app/core/config.py
@@ -0,0 +1,25 @@
+from pydantic_settings import BaseSettings
+from pydantic import AnyHttpUrl
+from typing import List
+
+
+class Settings(BaseSettings):
+    APP_NAME: str = "Kenya SME Predictor"
+    DEBUG: bool = False
+    FRONTEND_URL: str = "http://localhost:5173"
+
+    DATABASE_URL: str
+
+    SECRET_KEY: str
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    REFRESH_TOKEN_EXPIRE_DAYS: int = 7
+    PORT: int = 8000
+
+    class Config:
+        env_file = ".env"
+        case_sensitive = True
+        extra = "ignore"
+
+
+settings = Settings()
