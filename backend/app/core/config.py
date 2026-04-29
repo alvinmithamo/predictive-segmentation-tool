d8ab31595776d69109bcf961010dc81249a409c5
mithamoalvin <ngangaalvin100@gmail.com>
2026-04-29 23:21:26 +0300

    Add default algorithm setting to configuration

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -11,6 +11,7 @@ class Settings(BaseSettings):
     DATABASE_URL: str
 
     SECRET_KEY: str
+    ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
     REFRESH_TOKEN_EXPIRE_DAYS: int = 7
     PORT: int = 8001
